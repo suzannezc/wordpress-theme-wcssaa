a0e20ec3e4bdb455347cd8782c6e764cb6999014
James Schumann <jschumann@gmail.com>
2016-09-29 14:12:45 -0400

    admin ui lookin good

---
+++ taxonomy-venues.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div class="container">
+<div class="row">
+<div class="col-sm-12 col-lg-12">
+
+<h1><?php echo get_queried_object()->name; ?></h1>
+
+</div> <!-- end content area -->
+</div>
+</div>
+
+<?php get_footer();
